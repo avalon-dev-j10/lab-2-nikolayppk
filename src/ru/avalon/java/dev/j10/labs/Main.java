5e492ce9aaea565317ac6fb1103b0f3c4589a661
nikolayppk <nikolay.ppk@mail.ru>
2019-03-24 01:46:17 +0300

    save

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,11 +1,16 @@
 package ru.avalon.java.dev.j10.labs;
 
 import ru.avalon.java.dev.j10.labs.shapes.Shape;
+import ru.avalon.java.dev.j10.labs.shapes.Circle;
 
 public class Main {
 
     public static void main(String[] args) {
-        Shape[] shapes = null;
+        Shape[] shapes = new Shape [10];
+        shapes[0] = new Circle(3);
+        shapes[1] = new Circle(4);
+                
+        System.out.println(shapes[0].getArea());
 
         /*
          * TODO: Выполнить действия над массивом 'shapes'
